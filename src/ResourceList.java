5d0b425f1c67a08bb7792c8383ae8472bf892bc3
cjc523 <jc3413@imperial.ac.uk>
2017-04-06 02:43:49 +1000

    add resourceList class, not working, got nullPointerException

---
+++ src/ResourceList.java
@@ -0,0 +1,52 @@
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * Created by Peter on 6/4/17.
+ */
+public class ResourceList {
+    private HashMap<String, HashMap<String, HashMap<String, Resource>>> resources;
+
+    public void ResourceList() {
+        resources = new HashMap<String, HashMap<String, HashMap<String, Resource>>>();
+    }
+
+    public void put(Resource res) {
+        String channel = res.getChannel();
+        String owner = res.getOwner();
+        String uri = res.getUri();
+
+        /*
+        HashMap<String, Resource> r = new HashMap<String, Resource>();
+        HashMap<String, HashMap<String, Resource>> uris = new HashMap<String, HashMap<String, Resource>>();
+        r.put(owner, res);
+        uris.put(uri, r);
+        */
+
+        resources.put(channel, new HashMap<String, HashMap<String, Resource>>());
+        resources.get(channel).put(uri, new HashMap<String, Resource>());
+        resources.get(channel).get(uri).put(owner, res);
+    }
+
+    public Resource get(String owner, String channel, String uri) {
+        return resources.get(channel).get(uri).get(owner);
+    }
+
+    public boolean contains(Resource res) {
+        if(!resources.isEmpty()) {
+            System.out.println(1);
+            if (resources.containsKey(res.getChannel())) {
+                System.out.println(2);
+                if (resources.get(res.getChannel()).containsKey(res.getUri())) {
+                    System.out.println(3);
+                    if (!resources.get(res.getChannel()).get(res.getUri()).containsKey(res.getOwner())) {
+                        System.out.println(4);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,7 @@
 /** Project description to be added.
  *
  */
-import org.json.simple.*;
+//import org.json.simple.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,11 +10,30 @@ import java.util.Iterator;
 
 public class Server {
 
-    private HashMap<String[], Resource> resources = new HashMap<String[], Resource>();
-    private HashMap<String[], String[]> primaryKeys = new HashMap<String[], String[]>();
-    private int uri = 2;
-    private int channel = 1;
-    private int owner = 0;
+    //private static ArrayList<Resource> res = new ArrayList<Resource>();
+    private static ResourceList resources = new ResourceList();
+
+    /**
+     * testing main
+     */
+    public static void main(String[] args) {
+
+        JSONObject abc = new JSONObject();
+        abc.put("name", "ezshare");
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("cmd");
+        arr.add("win");
+        abc.put("tags", arr);
+        abc.put("description", "");
+        abc.put("uri", "http://abc.com");
+        abc.put("channel", "");
+        abc.put("owner", "");
+        abc.put("ezserver", "");
+
+
+        publish(abc);
+        System.out.println(resources.toString());
+    }
 
     /**
      * publish resource in the server
@@ -22,30 +41,34 @@ public class Server {
      * @param obj
      * json object contain the resource
      */
-    public void publish(JSONObject obj) {
+    public static void publish(JSONObject obj) {
         //json object contains command respond
         JSONObject response = new JSONObject();
-        String[] keys = getPrimaryKeys(obj);
-
-        //key contains only channel and uri
-        String[] pKey = new String[2];
-        pKey[0] = keys[channel];
-        pKey[1] = keys[uri];
+        String uri = (String) obj.get("uri");
+        String owner = (String) obj.get("owner");
+        String channel = (String) obj.get("channel");
 
         //check if resource is valid
-        if(keys[uri].equals("")
-                || keys[uri] == null
-                || keys[owner].equals("*")
-                || primaryKeys.containsKey(pKey)) {
+        if(uri.equals("")
+                || uri == null
+                || owner.equals("*")) {
             response.put("response", "error");
             response.put("errorMessage", "invalid resource");
-        } else if(keys[uri].startsWith("file:\\/\\/\\/")) {
+        } else if(uri.startsWith("file:\\/\\/\\/")) {
             response.put("response", "error");
             response.put("errorMessage", "cannot publish resource");
         } else {
             //create a resource and add to the resource list
-            //Resource res = addResource(obj, keys, pKey);
-            response.put("response", "success");
+            Resource res = getResource(obj);
+            //if(!resources.contains(res)) {
+            if(true){
+                resources.put(res);
+                response.put("response", "success");
+                System.out.println(res.toJSON());
+            } else {
+                response.put("response", "error");
+                response.put("errorMessage", "invalid resource");
+            }
         }
 
         //respond to command
@@ -58,97 +81,74 @@ public class Server {
      * @param obj
      * json object contain the resource
      */
-    public void share(JSONObject obj) {
+    public static void share(JSONObject obj) {
         //json object contains command respond
         JSONObject response = new JSONObject();
-        String[] keys = getPrimaryKeys(obj);
 
-        //key contains only channel and uri
-        String[] pKey = new String[2];
-        pKey[0] = keys[channel];
-        pKey[1] = keys[uri];
+        String uri = (String) obj.get("uri");
+        String owner = (String) obj.get("owner");
 
         //check if resource is valid
-        if(keys[uri].equals("")
-                || keys[uri] == null
-                || keys[owner].equals("*")
-                || primaryKeys.containsKey(pKey)) {
+        if(uri.equals("")
+                || uri == null
+                || owner.equals("*")) {
             response.put("response", "error");
             response.put("errorMessage", "invalid resource");
-        } else if(!keys[uri].startsWith("file:\\/\\/\\/")) {
+        } else if(!uri.startsWith("file:\\/\\/\\/")) {
             response.put("response", "error");
             response.put("errorMessage", "cannot share resource");
         } else {
             //create a resource and add to the resource list
-            //Resource res = addResource(obj, keys, pKey);
-            response.put("response", "success");
+            Resource res = getResource(obj);
+            if(!resources.contains(res)) {
+                resources.put(res);
+                response.put("response", "success");
+                System.out.println(res.toJSON());
+            } else {
+                response.put("response", "error");
+                response.put("errorMessage", "invalid resource");
+            }
         }
 
         //respond to command
         System.out.println(response);
     }
 
-    /**
-     * get the primary key of resource
-     *
-     * @param obj
-     * json object contain the resource
-     *
-     * @return
-     * primary keys
-     */
-    private String[] getPrimaryKeys(JSONObject obj) {
-        //primary keys
-        String uri = (String) obj.get("uri");
-        String channel = (String) obj.get("channel");
-        String owner = (String) obj.get("owner");
-        String[] keys = new String[3];
-        keys[0] = owner;
-        keys[1] = channel;
-        keys[2] = uri;
-
-        return keys;
-    }
 
     /**
      * parse json object into a resource object
      *
      * @param obj
      * json object contain the resource
-     *
-     * @param keys
-     * primary keys, 0 is owner, 1 is channel, 2 is uri
      */
-    private Resource addResource(JSONObject obj, String[] keys, String[] pKey) {
+    private static Resource getResource(JSONObject obj) {
 
 
         //new resource with uri
-        Resource res = new Resource(keys[uri]);
+        Resource res = new Resource((String)obj.get("uri"));
 
         //array to store tags
-        ArrayList<String> tags = new ArrayList<String>();
+        //ArrayList<String> tags = new ArrayList<String>();
 
         //set variables in Resource
         res.setName((String) obj.get("name"));
-        res.setChannel(keys[channel]);
+        res.setChannel((String)obj.get("channel"));
         res.setDescription((String) obj.get("description"));
-        res.setOwner(keys[owner]);
+        res.setOwner((String)obj.get("owner"));
         res.setOwner((String) obj.get("ezserver"));
 
         //iterate through the tags array and set the tags in Resource
-        JSONArray arr = (JSONArray) obj.get("tags");
-        Iterator it = arr.iterator();
+        ArrayList<String> arr = (ArrayList<String>) obj.get("tags");
+        /*Iterator it = arr.iterator();
 
         while (it.hasNext()) {
             tags.add((String) it.next());
         }
         if (!tags.isEmpty()) {
             res.setTags(tags);
-        }
-
+        }*/
+        res.setTags(arr);
         //add resource to resource list
-        primaryKeys.put(pKey, keys);
-        resources.put(keys, res);
         return res;
     }
 
