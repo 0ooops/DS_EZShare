5ce323d957bcd7ab42d95b63227c6d7d186502fd
Jiayu <jiayu_fibby@hotmail.com>
2017-05-12 21:13:10 +1000

    Add key store

--- src/main/java/EZShare/Subscribe.java
+++ src/main/java/EZShare/Subscribe.java
@@ -2,8 +2,8 @@ package EZShare;
 
 /**
  * This class is used for subscribing and unsubscribing functions on EZShare System.
- * @author: Jiayu Wang
- * @date: May 10, 2017
+ * @author:
+ * @date: May 12, 2017
  */
 
 import net.sf.json.JSONObject;
@@ -18,19 +18,37 @@ import java.util.logging.Logger;
 public class Subscribe {
     public static void subscribe (JSONObject cmd, Socket clientSocket, HashMap<Integer, Resource>
             resourceList, Boolean secure, Logger logr_debug) throws IOException {
-        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-        HashMap<Integer, Resource> returnResouce = new HashMap<>();
-//        JSONObject stop;
 
-        while (true) {
+        //以下内容是我写着玩的，随便你们删
+//        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+//        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+//        HashMap<Integer, Resource> returnResouce = new HashMap<>();
+////        JSONObject stop;
+//
+//        while (true) {
 //            if (in.available() > 0) {
 //                stop = JSONObject.fromObject(in.readUTF());
 //                if (stop.containsKey("command") && stop.containsKey("id")) {
 //                    stop.get("command").toString().equals("UNSUBSCRIBE")
 //                }
 //            }
+//        }
+
+        if (cmd.containsKey("resourceTemplate")) {
+
+        } else {
+
         }
+    }
+
+    public static void selfSubscribe (JSONObject cmd, Socket clientSocket, HashMap<Integer, Resource>
+            resourceList, Boolean secure, Logger logr_debug) {
 
     }
+
+    public static void otherSubscribe (JSONObject cmd, Socket clientSocket, HashMap<Integer, Resource>
+            resourceList, Boolean secure, Logger logr_debug) {
+
+    }
+
 }
